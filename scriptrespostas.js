c19ead7b6552d4200def3cf4ce7458d7279bf9f9
Pedro Camacho <pedroalmeidacamacho@gmail.com>
2022-06-02 22:54:52 -0300

    aaa

--- scriptrespostas.js
+++ scriptrespostas.js
@@ -1,5 +1,5 @@
 let score = 0
-function responder(valor){
+function responder1(valor){
    let next= document.getElementById("pergunta2");
    next.style.display="block";
    let resposta =document.querySelector("#score");
@@ -8,7 +8,48 @@ function responder(valor){
        alert("acertou");
        score++;  
     }  
+    let ultima=document.getElementById("pergunta1");
+    ultima.style.display="none";
 }
+
+function responder2(valor){
+   let next= document.getElementById("pergunta3");
+   next.style.display="block";
+   let resposta =document.querySelector("#score");
+		resposta.innerText=score;
+    if(valor==1){
+       alert("acertou");
+       score++;  
+    }  
+    let ultima=document.getElementById("pergunta2");
+    ultima.style.display="none";
+}
+
+function responder3(valor){
+   let next= document.getElementById("pergunta4");
+   next.style.display="block";
+   let resposta =document.querySelector("#score");
+		resposta.innerText=score;
+    if(valor==1){
+       alert("acertou");
+       score++;  
+    }  
+    let ultima=document.getElementById("pergunta3");
+    ultima.style.display="none";
+}
+function responder4(valor){
+   let next= document.getElementById("pergunta4");
+   next.style.display="block";
+   let resposta =document.querySelector("#score");
+		resposta.innerText=score;
+    if(valor==1){
+       alert("acertou");
+       score++;  
+    }  
+    let ultima=document.getElementById("pergunta3");
+    ultima.style.display="none";
+}
+   
    
     
    
